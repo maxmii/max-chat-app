4dfc411e15f88ee7b60607902218f560fd546d02
maxmii <morrm046@gmail.com>
2022-06-11 18:48:20 +0100

    Add events

--- src/index.js
+++ src/index.js
@@ -12,8 +12,18 @@ const publicDir = path.join(__dirname, '../public')
 
 app.use(express.static(publicDir))
 
-io.on('connection', () => {
+let count = 0
+
+io.on('connection', (socket) => {
     console.log('New Websocket connection')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
 })
 
 server.listen(port, () => {
