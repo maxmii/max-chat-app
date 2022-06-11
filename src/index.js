59f4b491f730e3b89a8b152951405e8e9ec2ab66
maxmii <morrm046@gmail.com>
2022-06-11 19:37:17 +0100

    Update changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const express = require('express')
 const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
 
 const app = express()
@@ -8,27 +8,20 @@ const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDir = path.join(__dirname, '../public') 
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicDir))
-
-let count = 0
+app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log('New Websocket connection')
+    console.log('New WebSocket connection')
 
-    // socket.emit('countUpdated', count)
-    // socket.emit('message', 'Welcome!')
+    socket.emit('message', 'Welcome!')
+
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
     })
-
-    // socket.on('increment', () => {
-    //     count++
-    //     io.emit('countUpdated', count)
-    // })
 })
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}`)
+    console.log(`Server is up on port ${port}!`)
 })
